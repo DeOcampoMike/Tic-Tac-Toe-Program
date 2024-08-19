4acd13c79d80c6d1e8fd9655abf35b3720e406a4
DiakosiMike <dmikenazareth@gmail.com>
2024-08-19 19:47:08 +0800

    For Setting Tiles

--- Tileset.py
+++ Tileset.py
@@ -1,18 +1,22 @@
 import checkwinner
 from checkwinner import *
-from twoplayer import *
 import twoplayer
+from minimax import best_move
+from variable import *
 
-def set_tile(row,column):
-        global currplayer
+def set_tile(row, column):
+    import variable
+    global currplayer, game_over  # Declare both as global
+    print("2")
 
+    if variable.gamemode == 0:  # Two-player mode
         if game_over:
-                return
+            return
 
-        elif board[row][column]["text"] == "" and checkwinner.game_over == False:
+        elif board[row][column]["text"] == "" and not checkwinner.game_over:
                 board[row][column]["text"] = currplayer
 
-
+            # Check for a winner after the move
                 if currplayer == player2:
                         currplayer = player1
                 else:
@@ -25,3 +29,23 @@ def set_tile(row,column):
 
         else:
                return
+
+    else:  # Single-player mode
+        if game_over:
+            return
+
+        if board[row][column]["text"] == "" and currplayer == "X":
+            board[row][column]["text"] = currplayer
+            twoplayer.label2["text"] = f"{currplayer}'s turn"
+
+            # Check for a winner after the player's move
+            check_winner(board, twoplayer.label2)
+            currplayer = "O"
+            move = best_move()
+            print(f"Best move determined by AI: {move}")  # Debugging output
+            if move:
+                board[move[0]][move[1]]["text"] = currplayer
+                check_winner(board, twoplayer.label2)
+                currplayer = "X"
+                twoplayer.label2["text"] = f"{currplayer}'s turn"
+        return
